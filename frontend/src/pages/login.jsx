573fd7866b9af211a4a783ebea8c632518aed74c
Shruti More <shrutimore820@gmail.com>
2026-06-15 17:44:27 +0530

    Added login page

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+
+    // Call Spring Boot Login API here
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1>TaskFlow</h1>
+        <p>Project Management & Team Collaboration</p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-btn">
+            Sign In
+          </button>
+        </form>
+
+        <div className="extra-links">
+          <a href="/">Forgot Password?</a>
+          <span> | </span>
+          <a href="/">Create Account</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
