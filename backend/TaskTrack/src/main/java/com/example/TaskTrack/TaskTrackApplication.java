41ad942c641cbe794d790559a09aee94d86b9d8e
Shruti More <shrutimore820@gmail.com>
2026-06-15 17:15:31 +0530

    inital commit

---
+++ backend/TaskTrack/src/main/java/com/example/TaskTrack/TaskTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackApplication.class, args);
+	}
+
+}
